42f9df0e77dda56e6da1665bdcbab42ae81e3aa0
Arun Ramaswamy <arun@Aruns-MacBook-Pro.local>
2017-04-05 22:22:48 -0500

    Initial Commit

---
+++ Basic-CalculatorTests/Basic_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Basic_CalculatorTests.swift
+//  Basic-CalculatorTests
+//
+//  Created by Arun Ramaswamy on 4/5/17.
+//  Copyright © 2017 Arun Ramaswamy. All rights reserved.
+//
+
+import XCTest
+@testable import Basic_Calculator
+
+class Basic_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
